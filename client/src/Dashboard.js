9755e91ab27a3f5f275018d17d3a769feb55e301
shawn-nyk <shawnngyongkwang@gmail.com>
2021-11-13 01:43:58 +0800

    Add basic game functionality

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import useAuth from "./useAuth";
 import TrackSearchResult from "./TrackSearchResult";
-import { Container, Form } from "react-bootstrap";
+import { Container, Form, Button } from "react-bootstrap";
 import SpotifyWebApi from "spotify-web-api-node";
 
 const spotifyApi = new SpotifyWebApi({
@@ -10,59 +10,109 @@ const spotifyApi = new SpotifyWebApi({
 
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
-  const [search, setSearch] = useState("");
+  // const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [showResult, setShowResult] = useState(false);
 
   useEffect(() => {
     if (!accessToken) return;
     spotifyApi.setAccessToken(accessToken);
   }, [accessToken]);
 
-  useEffect(() => {
-    if (!search) return setSearchResults([]);
-    if (!accessToken) return;
+  // useEffect(() => {
+  //   if (!search) return setSearchResults([]);
+  //   if (!accessToken) return;
 
-    let cancel = false;
-    spotifyApi.searchTracks(search).then((res) => {
-      if (cancel) return;
-      setSearchResults(
-        res.body.tracks.items.map((track) => {
-          const smallestAlbumImage = track.album.images.reduce(
-            (smallest, image) => {
-              if (image.height < smallest.height && image.height >= 160)
-                return image;
-              return smallest;
-            },
-            track.album.images[0]
-          );
+  //   let cancel = false;
+  //   spotifyApi
+  //     .getMySavedAlbums({
+  //       limit: 50,
+  //       offset: 0,
+  //     })
+  //     .then((res) => {
+  //       if (cancel) return;
+  //       setSearchResults(
+  //         res.body.items.map((item) => {
+  //           const smallestAlbumImage = item.album.images.reduce(
+  //             (smallest, image) => {
+  //               if (image.height < smallest.height && image.height >= 160)
+  //                 return image;
+  //               return smallest;
+  //             },
+  //             item.album.images[0]
+  //           );
 
-          return {
-            artist: track.artists[0].name,
-            title: track.name,
-            uri: track.uri,
-            albumUrl: smallestAlbumImage.url,
-          };
-        })
-      );
-    });
+  //           return {
+  //             artist: item.album.artists[0].name,
+  //             title: item.album.name,
+  //             uri: item.album.uri,
+  //             albumUrl: smallestAlbumImage.url,
+  //           };
+  //         })
+  //       );
+  //     });
 
-    return () => (cancel = true);
-  }, [search, accessToken]);
+  //   return () => (cancel = true);
+  // }, [search, accessToken]);
+
+  const onClickHandler = () => {
+    setShowResult(false);
+    const randomOffset = Math.floor(Math.random() * 3000);
+    spotifyApi
+      .getMySavedAlbums({
+        limit: 1,
+        offset: randomOffset,
+      })
+      .then((res) => {
+        setSearchResults(
+          res.body.items.map((item) => {
+            const smallestAlbumImage = item.album.images.reduce(
+              (smallest, image) => {
+                if (image.height < smallest.height && image.height >= 160)
+                  return image;
+                return smallest;
+              },
+              item.album.images[0]
+            );
+
+            return {
+              artist: item.album.artists[0].name,
+              title: item.album.name,
+              uri: item.album.uri,
+              albumUrl: smallestAlbumImage.url,
+            };
+          })
+        );
+      });
+  };
+
+  const showResultHandler = () => {
+    setShowResult(true);
+  };
 
   return (
     <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
-      <Form.Control
+      {/* <Form.Control
         type="search"
         placeholder="Search Songs/Artists"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-      />
+      /> */}
       <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
         {searchResults.map((track) => (
-          <TrackSearchResult track={track} key={track.uri} />
+          <TrackSearchResult
+            track={track}
+            showResult={showResult}
+            key={track.uri}
+          />
         ))}
       </div>
-      <div>Bottom</div>
+      <Button variant="primary" onClick={onClickHandler}>
+        Get An Album
+      </Button>
+      <Button variant="secondary" onClick={showResultHandler}>
+        Show Result
+      </Button>
     </Container>
   );
 }
--- client/src/TrackSearchResult.js
+++ client/src/TrackSearchResult.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Pixelify } from "react-pixelify";
 
-export default function TrackSearchResult({ track }) {
+export default function TrackSearchResult({ track, showResult }) {
   return (
     <div className="d-flex m-2 align-items-center">
-      <img
+      {/* <img
         src={track.albumUrl}
         style={{ height: "64px", width: "64px" }}
         alt="album art"
@@ -12,8 +12,19 @@ export default function TrackSearchResult({ track }) {
       <div className="ml-3">
         <div>{track.title}</div>
         <div className="text-muted">{track.artist}</div>
-      </div>
-      {/* <Pixelify src={track.albumUrl} pixelSize={16} width={160} height={160} /> */}
+      </div> */}
+      <Pixelify
+        src={track.albumUrl}
+        pixelSize={showResult ? 1 : 16}
+        width={160}
+        height={160}
+      />
+      {showResult && (
+        <div className="ml-3">
+          <div>{track.title}</div>
+          <div className="text-muted">{track.artist}</div>
+        </div>
+      )}
     </div>
   );
 }
