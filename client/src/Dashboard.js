c401c1a76f5d7416ccfd800013f2ce888d72576b
shawn-nyk <shawnngyongkwang@gmail.com>
2021-11-14 22:47:22 +0800

    Suppress useEffect warning

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -49,6 +49,7 @@ export default function Dashboard({ code }) {
   useEffect(() => {
     if (!accessToken) return;
     getNumSavedAlbums(0);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [accessToken]);
 
   // useEffect(() => {
